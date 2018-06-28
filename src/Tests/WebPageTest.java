6f0a6d5b1bacf839e99b94c144f0a53629110266
sugz <isugars64@gmail.com>
2018-06-27 20:42:33 -0700

    Working on unit testing

--- src/Tests/WebPageTest.java
+++ src/Tests/WebPageTest.java
@@ -1,9 +1,16 @@
 package Tests;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matcher.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.HashMap;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -37,30 +44,68 @@ public class WebPageTest {
 		assertEquals(page.urlObject.toString(), baseUrl);
 	}
 	
+//	@Test
+//	public void whenHeadersGivengetDocumentUrlCorrectTest() {
+//		String baseUrl = "https://seattle.craigslist.org/d/housing/search/hhh";
+//		HashMap<String, String> searchParams = new HashMap<>();
+//		searchParams.put("postal", "98144");
+//		searchParams.put("max_price", "3000");
+//		searchParams.put("min_bedrooms", "2");
+//		searchParams.put("min_bathrooms", "1");
+//		String correctUrl = "https://seattle.craigslist.org/search/hhh?max_price=3000&min_bathrooms=1&min_bedrooms=2&postal=98144";
+//		Webpage page = new Webpage(baseUrl);
+//		Document doc = page.getDocument(baseUrl, searchParams);
+//		String url = doc.absUrl(baseUrl);
+//		System.out.println("constructed url: " + doc.baseUri() + "\nactual url: " + correctUrl);
+////		assertEquals(url, correctUrl);
+//		fail("how do I get the full url with jsoup?");
+//	}
+	
 	@Test
-	public void whenHeadersGivengetConnectionReturnsCorrectUrlTest() {
-		String baseUrl = "https://seattle.craigslist.org";
+	public void whenNoHeadersgetConnectionReturnsCorrectUrlTest() {
+		String baseUrl = "https://seattle.craigslist.org/d/housing/search/hhh";
+		HashMap<String, String> searchParams = new HashMap<>();
+		Webpage page = new Webpage(baseUrl);
+		Document doc = page.getDocument(baseUrl, searchParams);
+		String url = doc.baseUri();
+		assertEquals(url, baseUrl);
+	}
+	
+	// Test that the document returned contains only items that fit within search parameters
+	@Test
+	public void documentRetrievedContainsCorrectPostalCodesTest() {
+		String baseUrl = "https://seattle.craigslist.org/d/housing/search/hhh";
 		HashMap<String, String> searchParams = new HashMap<>();
 		searchParams.put("postal", "98144");
 		searchParams.put("max_price", "3000");
 		searchParams.put("min_bedrooms", "2");
 		searchParams.put("min_bathrooms", "1");
-		String correctUrl = "https://seattle.craigslist.org/search/hhh?postal=98144&max_price=3000&min_bedrooms=3&min_bathrooms=1&availabilityMode=0&sale_date=all+dates";
-		// https://seattle.craigslist.org/max_price=3000&min_bedrooms=2&postal=98144&min_bathrooms=1&
-
 		Webpage page = new Webpage(baseUrl);
-		String url = page.getConnectionUrl(searchParams);
-		System.out.println(url);
-		assertEquals(url, correctUrl);
+		Document doc = page.getDocument(baseUrl, searchParams);
+		int maxPrice = 3000;
+		// check all postal codes
+		Elements elements = doc.select("p.result-info > span.result-price");
+		 for (Element e : elements) {
+//		      assertThat(Integer.parseInt(e.text()),lessThan(3000));
+			 System.out.println("price: " + e.text());
+			 int htmlPrice = Integer.parseInt(e.text());
+			 assertTrue(htmlPrice <= maxPrice);
+		 }
 	}
-	
-	@Test
-	public void whenNoHeadersgetConnectionReturnsCorrectUrlTest() {
-		String baseUrl = "https://seattle.craigslist.org";
-		HashMap<String, String> searchParams = new HashMap<>();
-		Webpage page = new Webpage(baseUrl);
-		String url = page.getConnectionUrl(searchParams);
-		assertEquals(url, baseUrl);
-	}
+//	
+//	@Test
+//	public void documentRetrievedContainsCorrectMaximumPriceTest() {
+//		fail("not implemented");
+//	}
+//	
+//	@Test
+//	public void documentRetrievedContainsCorrectMinimumBedroomsTest() {
+//		fail("not implemented");
+//	}
+//	
+//	@Test void documentRetrievedContainsCorrectMinimumBathroomsTest() {
+//		fail("not implemented");
+//	}
+//	
 
 }
--- src/com/isugz/ScraperTools/Webpage.java
+++ src/com/isugz/ScraperTools/Webpage.java
@@ -58,65 +58,17 @@ public class Webpage {
 	}
 	
 	/**
-	 * Method constructs HTTP connection using urlObject.
-	 * @param searchParams: String array listing headers to be included with final URL, if any.
-	 * @return: String representing the final URL for connection.
-	 */
-	public String getConnectionUrl(HashMap<String, String> searchParams) {
-		URL urlWithHeaders = null;
-		try {
-			HttpURLConnection connection = (HttpURLConnection) urlObject.openConnection();
-			connection.connect();
-			if(!searchParams.isEmpty()) {			
-				String parameters = getHeaderString(searchParams, connection);
-				connection.disconnect();
-				try {
-					urlWithHeaders = new URL(url + "/search/hhs?" + parameters);
-					HttpURLConnection newConnection = (HttpURLConnection) urlWithHeaders.openConnection();
-					newConnection.connect();
-					return urlWithHeaders.toString();
-				}catch(MalformedURLException e) {
-					e.printStackTrace();
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return urlObject.toString();
-	}
-	
-	/**
-	 * Private helper method to get headers for URL.
-	 * @param searchParams: String array that include all parameters for final URL.
-	 * @param connection: Established HTTP connection.
-	 * @return: the constructed headers.
-	 * TODO this should just return a header string to add as the final part of the URL
-	 */
-	private String getHeaderString(HashMap<String, String> searchParams, HttpURLConnection connection) {
-		StringBuilder resultString = new StringBuilder();
-		try {
-			for(Map.Entry<String, String> entry : searchParams.entrySet()) {
-	          resultString.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-	          resultString.append("=");
-	          resultString.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
-	          resultString.append("&");
-			}
-		}catch(UnsupportedEncodingException e) {
-				e.printStackTrace();
-			}
-		return resultString.toString();
-	}
-	
-	/**
 	 * Public method to retrieve document from given URL.
 	 * @param url: String representing final version of request URL.
 	 * @return: Document (HTML) retrieved from URL.
 	 */
-	public Document getDocument(String url) {
+	public Document getDocument(String url, HashMap<String, String> headers) {
 		Document document = null;
 		try {
-			document = Jsoup.connect(url).get();
+			document = Jsoup.connect(url).headers(headers).get();
+			
 			// TODO Jsoup.connect is designed for method chaining; address cookies here
+			System.out.println(document.html());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
