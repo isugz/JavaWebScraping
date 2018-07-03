5d649835cacddc0d960c5023d28e861e1beb411a
sugz <isugars64@gmail.com>
2018-07-03 12:15:40 -0700

    implemented some scraping for now; need to write tests and decide what direction to go in

--- src/Tests/ScraperTest.java
+++ src/Tests/ScraperTest.java
@@ -5,11 +5,19 @@ package Tests;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.Connection;
+import org.jsoup.nodes.Document;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.isugz.JavaWebScraper.Scraper;
+import com.isugz.ScraperTools.Webpage;
 
 /**
  * @author Ivy Sugars
@@ -47,7 +55,21 @@ public class ScraperTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		String baseUrl = "https://seattle.craigslist.org/d/housing/search/hhh";
+		Webpage page = new Webpage(baseUrl);
+		Map<String, String> searchParams = new HashMap<>();
+		searchParams.put("postal", "98144");
+		searchParams.put("max_price", "3000");
+		searchParams.put("min_bedrooms", "3");
+		searchParams.put("min_bathrooms", "1");
+		Connection connection = page.getJsoupConnection(baseUrl, searchParams);
+		Document doc = page.getDocument(connection);
+		Scraper scraper = new Scraper(doc);
+		String[] requestedData = {"housing", "result-price", "result-hood"};
+		String contentId = "sortable-results";
+		String contentClass = "result-meta";
+		scraper.scrape(requestedData, contentId, contentClass);
+//		fail("Not yet implemented");
 	}
 
 }
--- src/com/isugz/JavaWebScraper/Scraper.java
+++ src/com/isugz/JavaWebScraper/Scraper.java
@@ -3,9 +3,15 @@
  */
 package com.isugz.JavaWebScraper;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import com.isugz.ScraperTools.Webpage;
 
 /**
  * @author Ivy Sugars
@@ -37,36 +43,24 @@ public class Scraper {
 	 * TODO look at making a request object that holds the list of data to scrape from a document
 	 * 		and the html id's, tags, & classes to use
 	 */
-	public Object scrape(String[] requestedData, String contentId, String contentTag) {
-//		JsonObject jsonString = new JsonObject();
-		StringBuilder jsonString = new StringBuilder();
+	public String scrape(String[] requestedData, String contentId, String contentClass) {
+		StringBuilder resultString = new StringBuilder();
 		Element resultContent = this.document.getElementById(contentId);
-		Elements resultListItems = resultContent.getElementsByTag(contentTag);
-		for(Element item : resultListItems) {
-			// iterate through the requestData, adding to a JSON formatted string
-			for(String request : requestedData) {
-			}
+		Elements resultItems = resultContent.getElementsByClass(contentClass);
+		for(Element item : resultItems) {
+			resultString.append("{");
+			resultString.append(":");
+			String houseInformation = item.getElementsByClass(requestedData[0]).text();
+			String price = item.getElementsByClass(requestedData[1]).text();
+			String neighborhood = item.getElementsByClass(requestedData[2]).text();
+			System.out.println(houseInformation + "\t" + price + "\t" + neighborhood);
+			resultString.append(houseInformation);
+			resultString.append(price);
+			resultString.append(neighborhood);
+			resultString.append("}");
+			
 		}
-		/**
-		 * traverse the html
-		 * pull data that matches requestedData elements
-		 * 
-		 * 
-		 * we are looking for the <ul>
-		 * we want to loop through the <li> below
-		 * 
-		 * 
-		 * for each result in html
-		 * 		get requested info
-		 * return info-->text?
-		 */
-		return null;
+		System.out.println(resultString.toString());
+		return resultString.toString();
 	}
-	
-	/**
-	 * Find real estate web pages
-	 * Scrape for data
-	 * 
-	 */
-	
 }
